41047e1ab6b30596e9aeb522c43cfdd12ae872d1
hermes curo <hermescurov5@gmail.com>
2025-06-02 13:00:27 -0500

    adding new marker

---
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -0,0 +1,64 @@
+package com.example.lab12
+
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.BitmapDescriptor
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Marker
+
+@Composable
+fun MapScreen() {
+    val arequipaLocation = LatLng(-16.4040102, -71.559611)
+    val cameraPositionState = rememberCameraPositionState {
+        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(
+            arequipaLocation,
+            12f
+        )
+    }
+
+    val context = LocalContext.current
+    var customMarkerIcon by remember { mutableStateOf<BitmapDescriptor?>(null) }
+
+    var mapLoaded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                mapLoaded = true
+            }
+        ) {
+            // Solo creamos el icono cuando el mapa ya está cargado
+            if (mapLoaded && customMarkerIcon == null) {
+                customMarkerIcon = BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.icons8_mountain_48
+                    )
+                )
+            }
+
+            // Dibujamos el marcador solo cuando el icono está listo
+            customMarkerIcon?.let { icon ->
+                Marker(
+                    state = rememberMarkerState(position = arequipaLocation),
+                    icon = icon,
+                    title = "Arequipa, Perú"
+                )
+            }
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-}
-buildscript {
-    dependencies {
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
+    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
